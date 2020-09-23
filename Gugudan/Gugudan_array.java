332679454dba29468b918d049c45177b2d981688
SungYoonCho <sungyoon606@naver.com>
2020-09-23 14:04:32 +0900

    Gugudan using array

---
+++ Gugudan/Gugudan_array.java
@@ -0,0 +1,16 @@
+
+public class Gugudan_array {
+
+	public static void main(String[] args) {
+		int[] result = new int[9];
+		for (int j = 2; j < 10; j++) {
+			for (int i = 0; i < result.length; i++) {
+				result[i] = j * (i + 1);
+			}
+			for (int i = 0; i < result.length; i++) {
+				System.out.println(result[i]);
+			}			
+		}
+	}
+
+}
