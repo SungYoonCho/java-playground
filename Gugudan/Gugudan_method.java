61984e43a8bfbebfa13d77744e721834a0c1e701
SungYoonCho <sungyoon606@naver.com>
2020-09-24 15:16:55 +0900

    Gugudan by method

---
+++ Gugudan/Gugudan_method.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Gugudan_method {
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		
+		for (int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("몇 단을 출력하시겠습니까? : ");
+		Scanner scanner = new Scanner(System.in);
+		int times = scanner.nextInt();
+		
+		int[] result = calculate(times);
+		print(result);
+	}
+}
