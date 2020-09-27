25b8889fd40c73c2c9c4c6af7a04ce918221c772
veluminous <sungyoon606@naver.com>
2020-09-27 18:42:31 +0900

    Gugudan plus mission 1

---
+++ Gugudan/Gugudan_plus1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Gugudan_plus1 {
+	public static int[] calculate(int times, int n) {
+		int[] result = new int[n]; 
+		for (int i = 0; i < n; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("몇 단을 출력하시겠습니까? : ");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		for (int i = 2; i < (n+1); i++) {
+			int[] result = calculate(i, n);
+			print(result);			
+		}
+	}
+}
