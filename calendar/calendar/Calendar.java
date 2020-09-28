f26ce6b59d9bf52252adc0ac1f748a056c18aee8
veluminous <sungyoon606@naver.com>
2020-09-29 01:09:53 +0900

    finding max day of month - my solution

--- calendar/calendar/Calendar.java
+++ calendar/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,7 +11,23 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-
+		
+		
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] maxDays = {28, 30, 31};
+		if (n == 2) {
+			System.out.println(n + "월은 " + maxDays[0] + "까지 있습니다.");
+		}
+		else if (n % 2 == 0) {
+			System.out.println(n + "월은 " + maxDays[1] + "까지 있습니다.");
+		}
+		else if (n % 2 == 1){
+			System.out.println(n + "월은 " + maxDays[2] + "까지 있습니다.");
+		}
+		scanner.close();
 	}
 
 }
