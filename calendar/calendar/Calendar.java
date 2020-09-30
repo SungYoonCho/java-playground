8dc5669b2fe31e1914ab25454f87934ec496ad5a
veluminous <sungyoon606@naver.com>
2020-09-30 23:17:22 +0900

    Calendar loop

--- calendar/calendar/Calendar.java
+++ calendar/calendar/Calendar.java
@@ -23,10 +23,17 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요.");
+			int month = scanner.nextInt();
+			
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));			
+		}
+		
+		System.out.println("반복이 완료되었습니다.");
 		scanner.close();
 	}
 
