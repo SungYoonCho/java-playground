7beaec7bba55c6ef69697eea55c99587955e591d
veluminous <sungyoon606@naver.com>
2020-10-20 01:34:56 +0900

    Restructuring UI and functions.

--- calendar/calendar/Prompt.java
+++ calendar/calendar/Prompt.java
@@ -3,15 +3,79 @@ package calendar;
 import java.util.Scanner;
 
 public class Prompt {
+    
+	public static Scanner scanner = new Scanner(System.in);
+	
+	public void uiPrompt() {
+		Prompt prompt = new Prompt();
+		
+		prompt.printHelp();
+		
+		while (true) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			System.out.print("> ");
 
-	public void runPrompt() {
-		Scanner scanner = new Scanner(System.in);
+			char command = scanner.nextLine().charAt(0);
+			
+			if (command == '1') {
+				prompt.runPrompt1();
+			}
+			else if (command == '2') {
+				prompt.runPrompt2();
+			}
+			else if (command == '3') {
+				prompt.runPrompt3();
+			}
+			else if (command == 'h') {
+				prompt.printHelp();
+			}
+			else if (command == 'q') {
+				break;
+			}	
+		}
+		
+		System.out.println("Bye~!!!!");
+	}
+	
+	public void printHelp() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말       q. 종료");
+		System.out.println("+----------------------+");
+	}
+	
+	public void runPrompt1() {
+		Calendar cal = new Calendar();
+		
+		System.out.println("[일정 등록] 날짜를 입력하세요.");
+		System.out.print("> ");
+		String date = scanner.nextLine();
+		System.out.println("일정을 입력하세요.");
+		System.out.print("> ");			
+		String content = scanner.nextLine();
+		cal.saveSchedule(date, content);
+		System.out.println("일정이 등록되었습니다.");
+	}
+	
+	public void runPrompt2() {
+		Calendar cal = new Calendar();
+		
+		System.out.println("[일정 검색] 날짜를 입력하세요.");
+		System.out.print("> ");
+		String date = scanner.nextLine();
+		cal.printSchedule(date);
+	}
+	
+	public void runPrompt3() {
 		Calendar cal = new Calendar();
 
 		while (true) {			
 			System.out.println("년도를 입력하세요. (Exit : -1)");
 			System.out.print("Year> ");
 			int year = scanner.nextInt();
+			scanner.nextLine();
 			if (year == -1) {
 				break;
 			}
@@ -27,15 +91,11 @@ public class Prompt {
 //			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(year, month));
 			cal.printCalendar(year, month);
 		}
-
-		System.out.println("Bye~!!!!");
-		scanner.close();
-
 	}
 
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
-		p.runPrompt();
+		p.uiPrompt();
 	}
-
+	
 }
